f3e335eac5b819765a4af260bcffbe802c339953
Joshua Chappell <joshua.chappell@namely.com>
2018-09-26 10:31:18 -0400

    current vs code extensions

---
+++ code_extensions.ps1
@@ -0,0 +1,12 @@
+code --install-extension [createInstance] extensionManagementService depends on downloadService which is NOT registered.
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension eg2.vscode-npm-script
+code --install-extension ionutvmi.reg
+code --install-extension ms-mssql.mssql
+code --install-extension ms-vscode.csharp
+code --install-extension ms-vscode.Go
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension Orta.vscode-jest
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension zxh404.vscode-proto3
